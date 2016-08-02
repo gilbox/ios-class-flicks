3d1903ea9852cb109eff4d414e970aeebb7c5280
gilbox <birmangil@gmail.com>
2016-08-02 15:16:39 -0700

    refactor to use 2-space tabs

--- Flicks/DetailViewController.swift
+++ Flicks/DetailViewController.swift
@@ -10,50 +10,50 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var posterImageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var overviewLabel: UILabel!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var infoView: UIView!
-    
-    var movie: NSDictionary!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let title = movie["title"] as! String
-        let overview = movie["overview"] as! String
-        
-        titleLabel.text = title
-        overviewLabel.text = overview
-        overviewLabel.sizeToFit()
-        infoView.frame.size.height = overviewLabel.frame.size.height + overviewLabel.frame.origin.y + 10
-        
-        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: 60 + infoView.frame.origin.y + infoView.frame.size.height)
-        
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
-        
-        if let posterPath = movie["poster_path"] as? String {
-            let imageUrl = NSURL(string: baseUrl + posterPath)
-            posterImageView.setImageWithURL(imageUrl!)
-        }
-        // Do any additional setup after loading the view.
-    }
+  @IBOutlet weak var posterImageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var overviewLabel: UILabel!
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var infoView: UIView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+  var movie: NSDictionary!
 
-    /*
-    // MARK: - Navigation
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    let title = movie["title"] as! String
+    let overview = movie["overview"] as! String
+
+    titleLabel.text = title
+    overviewLabel.text = overview
+    overviewLabel.sizeToFit()
+    infoView.frame.size.height = overviewLabel.frame.size.height + overviewLabel.frame.origin.y + 10
+
+    scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: 60 + infoView.frame.origin.y + infoView.frame.size.height)
+
+    let baseUrl = "http://image.tmdb.org/t/p/w500"
+
+    if let posterPath = movie["poster_path"] as? String {
+      let imageUrl = NSURL(string: baseUrl + posterPath)
+      posterImageView.setImageWithURL(imageUrl!)
     }
-    */
+    // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+  /*
+   // MARK: - Navigation
+
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
 
 }
--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -12,104 +12,104 @@ import BFRadialWaveHUD
 
 class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var movies: [NSDictionary]?
-    var endpoint: String! // now_playing
-    var hud: BFRadialWaveHUD?
+  var movies: [NSDictionary]?
+  var endpoint: String! // now_playing
+  var hud: BFRadialWaveHUD?
 
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var errorView: UIView!
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var errorView: UIView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        hud = BFRadialWaveHUD(view: self.view, fullScreen: true, circles: BFRadialWaveHUD_DefaultNumberOfCircles, circleColor: nil, mode: BFRadialWaveHUDMode.KuneKune, strokeWidth: BFRadialWaveHUD_DefaultCircleStrokeWidth)
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        getData()
+    hud = BFRadialWaveHUD(view: self.view, fullScreen: true, circles: BFRadialWaveHUD_DefaultNumberOfCircles, circleColor: nil, mode: BFRadialWaveHUDMode.KuneKune, strokeWidth: BFRadialWaveHUD_DefaultCircleStrokeWidth)
 
-        // Do any additional setup after loading the view.
+    tableView.dataSource = self
+    tableView.delegate = self
+
+    getData()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return movies?.count ?? 0
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+    let movie = movies![indexPath.row]
+    let title = movie["title"] as! String
+    let overview = movie["overview"] as! String
+
+    let baseUrl = "http://image.tmdb.org/t/p/w500"
+
+    if let posterPath = movie["poster_path"] as? String {
+      let imageUrl = NSURL(string: baseUrl + posterPath)
+      cell.posterView.setImageWithURL(imageUrl!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies?.count ?? 0
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-        let movie = movies![indexPath.row]
-        let title = movie["title"] as! String
-        let overview = movie["overview"] as! String
-        
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
+    cell.titleLabel.text = title
+    cell.overviewLabel.text = overview
 
-        if let posterPath = movie["poster_path"] as? String {
-            let imageUrl = NSURL(string: baseUrl + posterPath)
-            cell.posterView.setImageWithURL(imageUrl!)
+    print("row \(indexPath.row)")
+    return cell
+  }
+
+  func getData() {
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
+    let request = NSURLRequest(
+      URL: url!,
+      cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+      timeoutInterval: 10)
+
+    let session = NSURLSession(
+      configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+      delegate: nil,
+      delegateQueue: NSOperationQueue.mainQueue()
+    )
+
+    hud?.show()
+
+    let task: NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {
+      (dataOrNil, response, error) in
+      if (error != nil) {
+        print("error \(error)")
+        self.errorView.hidden = false
+      }
+      else if let data = dataOrNil {
+        if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+          data, options:[]) as? NSDictionary {
+          print("response: \(responseDictionary)")
+
+          self.movies = responseDictionary["results"] as? [NSDictionary]
+          self.tableView.reloadData()
         }
-        
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview
-        
-        print("row \(indexPath.row)")
-        return cell
-    }
-    
-    func getData() {
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
-        let request = NSURLRequest(
-            URL: url!,
-            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
-            timeoutInterval: 10)
-        
-        let session = NSURLSession(
-            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
-            delegate: nil,
-            delegateQueue: NSOperationQueue.mainQueue()
-        )
-        
-        hud?.show()
-        
-        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {
-            (dataOrNil, response, error) in
-                if (error != nil) {
-                    print("error \(error)")
-                    self.errorView.hidden = false
-                }
-                else if let data = dataOrNil {
-                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                        data, options:[]) as? NSDictionary {
-                        print("response: \(responseDictionary)")
-                        
-                        self.movies = responseDictionary["results"] as? [NSDictionary]
-                        self.tableView.reloadData()
-                    }
-                }
-                self.hud?.dismiss()
-        })
-        task.resume()
-    }
+      }
+      self.hud?.dismiss()
+    })
+    task.resume()
+  }
 
-    // MARK: - Navigation
+  // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        print("prepare segue")
-        let cell = sender as! UITableViewCell
-        let indexPath = tableView.indexPathForCell(cell)
-        let movie = movies![indexPath!.row]
-        
-        let detailViewController = segue.destinationViewController as! DetailViewController
-        detailViewController.movie = movie
-    }
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    print("prepare segue")
+    let cell = sender as! UITableViewCell
+    let indexPath = tableView.indexPathForCell(cell)
+    let movie = movies![indexPath!.row]
+
+    let detailViewController = segue.destinationViewController as! DetailViewController
+    detailViewController.movie = movie
+  }
 
 }
